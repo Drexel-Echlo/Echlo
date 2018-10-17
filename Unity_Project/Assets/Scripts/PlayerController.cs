b02857d95f7346f3fc6a6fd7868f3a0cd591fdee
mart338 <mat338@drexel.edu>
2018-10-17 01:41:06 -0400

    Implemented food
    Food now exists. It can be collected by touching it, and it can be regurgitated in the home base by pressing SPACE.

--- Unity_Project/Assets/Scripts/PlayerController.cs
+++ Unity_Project/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public Rigidbody rb;
-    
+
+    public Transform mouth;
+
     public float horizontal;
     public float vertical;
     public float maxSpeed;
 
+    public SpriteRenderer spriteR;
+    public Sprite spriteNormal;
+    public Sprite spriteFull;
+    public GameObject food;
+
+    public bool isCarryingFood;
+
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,5 +30,41 @@ public class PlayerController : MonoBehaviour {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
         rb.velocity = new Vector3(horizontal * maxSpeed, rb.velocity.y, vertical * maxSpeed);
+
+        if (isCarryingFood)
+        {
+            spriteR.sprite = spriteFull;
+        }
+        else
+        {
+            spriteR.sprite = spriteNormal;
+        }
+
+    }
+
+    private void OnCollisionEnter (Collision other)
+    {
+        if (!isCarryingFood && other.gameObject.layer == 11)
+        {
+            isCarryingFood = true;
+            Destroy(other.gameObject);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void OnTriggerStay(Collider home)
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isCarryingFood)
+        {
+            isCarryingFood = false;
+            Instantiate(food, mouth.position, Quaternion.identity);
+        }
+        else
+        {
+            return;
+        }
     }
 }
