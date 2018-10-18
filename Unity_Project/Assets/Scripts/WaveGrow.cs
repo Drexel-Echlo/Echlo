ace24af34fd7ac01989f4226940f87e4d6937fa9
mart338 <mat338@drexel.edu>
2018-10-18 14:05:33 -0400

    Sound waves grow over time

---
+++ Unity_Project/Assets/Scripts/WaveGrow.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGrow : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        this.transform.localScale += new Vector3(Time.deltaTime / 2, Time.deltaTime / 2, 0);
+	}
+}
