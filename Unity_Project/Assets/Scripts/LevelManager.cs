d13e9c34f7855711c1452f684c93f70e242454a6
Mark <ms4339@drexel.edu>
2018-10-31 23:04:02 -0400

    UI Functionality - Try Playing off the StartScreen Scene

---
+++ Unity_Project/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public Button play;
+
+    private void Start()
+    {
+        play.onClick.AddListener(LoadLevel);
+    }
+    private void LoadLevel()
+    {
+        SceneManager.LoadScene("TechDemo", LoadSceneMode.Single);
+        Debug.Log("ButtonClicked");
+    }
+
+
+}
